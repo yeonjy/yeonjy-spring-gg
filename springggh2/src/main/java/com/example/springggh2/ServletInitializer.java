5990d7ed76f48c4c58b7d8b82f7af36ae001fbf0
yeonjy <yeonjypaper@gmail.com>
2022-10-12 02:30:22 +0900

    feat: h2 + JPA

---
+++ springggh2/src/main/java/com/example/springggh2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springggh2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Springggh2Application.class);
+    }
+
+}
---
+++ springggh2/src/main/java/com/example/springggh2/Springggh2Application.java
@@ -0,0 +1,13 @@
+package com.example.springggh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springggh2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springggh2Application.class, args);
+    }
+
+}
